294e4a30ea3a7b63289913d58a451d65fcaa2b57
macbookpro <jason@loblein.com>
2017-05-17 12:34:19 -0400

    initialize router

--- frontend/froliq.jsx
+++ frontend/froliq.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './util/session_api_util';
 import configureStore from './store/store';
-
-
-window.login = login;
-window.logout = logout;
-window.signup = signup;
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.store = configureStore();
-    const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to Froliq</h1>, root);
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
 });
