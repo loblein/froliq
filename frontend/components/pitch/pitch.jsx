cbd6ba404076de741496696f9af34a6eb9cf610e
macbookpro <jason@loblein.com>
2017-05-19 21:16:43 -0400

    outline pitch structure

--- frontend/components/pitch/pitch.jsx
+++ frontend/components/pitch/pitch.jsx
@@ -3,7 +3,30 @@ import React from 'react';
 const Pitch = () => {
   return (
     <div className='pitch container-fluid'>
-
+      <div className='row'>
+        <h2>Travel Free</h2>
+      </div>
+      <div className='row'>
+        <div className='col-md-2'></div>
+        <section className='col-md-8'>
+          <img src={window.travelImg}></img>
+        </section>
+        <div className='col-md-2'></div>
+      </div>
+      <div className='row'>
+        <div className='col-md-2'></div>
+        <section className='col-md-8'>
+          <img src={window.workImg} className='float-r'></img>
+        </section>
+        <div className='col-md-2'></div>
+      </div>
+      <div className='row'>
+        <div className='col-md-2'></div>
+        <section className='col-md-8'>
+          <img src={window.playImg}></img>
+        </section>
+        <div className='col-md-2'></div>
+      </div>
     </div>
   )
 }
