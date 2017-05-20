2da8d52578fbafaa25d372599b90c7c9be5e22e0
macbookpro <jason@loblein.com>
2017-05-19 22:38:35 -0400

    add placeholder text

--- frontend/components/pitch/pitch.jsx
+++ frontend/components/pitch/pitch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Pitch = () => {
   return (
@@ -7,7 +8,7 @@ const Pitch = () => {
         <div className='col-md-2'></div>
         <div className='col-md-8'>
           <div className='text-center'>
-            <span className='pitch-heading'>Froliq </span>
+              <span className='pitch-heading'>Froliq </span>
             <span className='pitch-sub-heading'>...and be free</span>
           </div>
         </div>
@@ -15,22 +16,28 @@ const Pitch = () => {
       </div>
       <div className='row'>
         <div className='col-md-2'></div>
-        <section className='col-md-8'>
+        <section className='col-md-8 clearfix'>
           <img src={window.travelImg}></img>
+          <h3>Explore</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
         </section>
         <div className='col-md-2'></div>
       </div>
       <div className='row'>
         <div className='col-md-2'></div>
-        <section className='col-md-8'>
+        <section className='col-md-8 clearfix'>
           <img src={window.workImg} className='float-r'></img>
+          <h3>Fund Your Travels</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
         </section>
         <div className='col-md-2'></div>
       </div>
       <div className='row'>
         <div className='col-md-2'></div>
-        <section className='col-md-8'>
+        <section className='col-md-8 clearfix'>
           <img src={window.playImg}></img>
+          <h3>Have an Adventure</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
         </section>
         <div className='col-md-2'></div>
       </div>
