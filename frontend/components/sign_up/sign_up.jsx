76a856c126ec49aeb02ea3d8c314e325b4a8ae3f
macbookpro <jason@loblein.com>
2017-05-23 21:42:56 -0400

    render basic signup page

--- frontend/components/sign_up/sign_up.jsx
+++ frontend/components/sign_up/sign_up.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../header/header';
+import FontAwesome from 'react-fontAwesome';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -11,10 +12,28 @@ class SignUp extends React.Component {
       <div className='container-fluid'>
         <div className='row'>
           <Header />
+
+        </div>
+
+        <div className='row'>
           <div className='sign-up'>
+            <div className='bi-fold-container'>
+              <div className='bi-fold-item'>
+                <h3>I am a</h3>
+                <FontAwesome className="fa fa-arrow-up fa-5x"></FontAwesome>
+                <h3>traveler</h3>
+              </div>
+
+              <div className='bi-fold-item'>
+                <h3>I am an</h3>
+                  <FontAwesome className="fa fa-arrow-down fa-5x"></FontAwesome>
+                <h3>employer</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
     )
   }
 }
