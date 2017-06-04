48ff8f7efdd3d8de3320ae9a686b5c6245cc8987
macbookpro <jason@loblein.com>
2017-06-04 08:49:12 -0400

    make main content flexible

--- frontend/components/user_profile/user_main/opportunities/opportunities.jsx
+++ frontend/components/user_profile/user_main/opportunities/opportunities.jsx
@@ -10,16 +10,21 @@ class Opportunities extends React.Component {
   render() {
     const jobs = _.values(this.props.jobs);
     const jobList = jobs.map((job, idx) => (
-      <li className='job-item' key={idx}>
+      <li className='job-item clearfix' key={idx}>
+        <div className='img-container'>
+          this should be image
+        </div>
         <h4>
           {job.title}
         </h4>
+        <h5>
+          ${job.rate}.00
+        </h5>
         <p>
           Description: {job.description}
         </p>
-        <p>
-          Rate: {job.rate}
-        </p>
+
+
       </li>
     ));
 
--- frontend/components/user_profile/user_main/user_main.jsx
+++ frontend/components/user_profile/user_main/user_main.jsx
@@ -14,39 +14,46 @@ class UserMain extends React.Component {
     return (
       <div className='container-fluid inspect'>
         <div className='row add-height'>
-          <div className='user-main-container'>
+          <div className='col-sm-0 col-md-1 col-lg-2'></div>
+          <div className='user-main-container col-md-10 col-sm-12 col-lg-8'>
             <div className='user-main'>
-              <nav>
-                <ul>
-                  <li>
-                    <FontAwesome className='fa fa-rocket' /> Opportunities
-                  </li>
-                  <li>
-                    <FontAwesome className='fa fa-hourglass-half' /> Pending
-                  </li>
-                  <li>
-                    <FontAwesome className='fa fa-check' /> Completed
-                    </li>
-                  <li>
-                    <FontAwesome className='fa fa-star' /> Ratings
-                  </li>
-                  <li>
-                    <FontAwesome className='fa fa-map' /> Map
-                  </li>
-                  <li>
-                    <FontAwesome className='fa fa-wrench' /> Settings
-                  </li>
-                </ul>
-              </nav>
-              <div className='dummy-nav'></div>
-              <div className='search-container'>
-                <SearchContainer />
+              <div className='searchbox-row'>
+                <div className='search-container'>
+                  <SearchContainer />
+                </div>
               </div>
-              <div className='content-box'>
-                <OpportunitiesContainer />
+              <div className='local-nav-row'>
+                <nav>
+                  <ul>
+                    <li>
+                      <FontAwesome className='fa fa-rocket' /> Opportunities
+                    </li>
+                    <li>
+                      <FontAwesome className='fa fa-hourglass-half' /> Pending
+                    </li>
+                    <li>
+                      <FontAwesome className='fa fa-check' /> Completed
+                      </li>
+                    <li>
+                      <FontAwesome className='fa fa-star' /> Ratings
+                    </li>
+                    <li>
+                      <FontAwesome className='fa fa-map' /> Map
+                    </li>
+                    <li>
+                      <FontAwesome className='fa fa-wrench' /> Settings
+                    </li>
+                  </ul>
+                </nav>
+                <div className='dummy-nav'></div>
+
+                <div className='content-box'>
+                  <OpportunitiesContainer />
+                </div>
               </div>
             </div>
           </div>
+          <div className='col-sm-0 col-md-1 col-lg-2'></div>
         </div>
         <div className='row'>
           <div className='user-footer-container'>
