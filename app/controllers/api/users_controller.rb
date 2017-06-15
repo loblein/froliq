a5bc65fbab781d9c345b9ba35454b98511c0f626
macbookpro <jason@loblein.com>
2017-06-15 11:27:41 -0400

    repair user update

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       login(@user)
       render '/api/users/show'
@@ -10,6 +10,16 @@ class Api::UsersController < ApplicationController
       render json: @user.errors.full_messages, status: 422
     end
 
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update_attributes(user_params)
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   def destroy
--- frontend/util/user_api_util.js
+++ frontend/util/user_api_util.js
@@ -1,10 +1,10 @@
 import { receiveCurrentUser, receiveErrors } from '../actions/session_actions';
 
 export const update = user => {
-  debugger
+
   return $.ajax({
     method: 'PATCH',
-    url: `api/users/:user_id`,
+    url: 'api/users/:id',
     data: user
   });
 };
