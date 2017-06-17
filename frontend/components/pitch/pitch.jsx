ca71745d0732db777175e3879cc948112354fc3e
macbookpro <jason@loblein.com>
2017-06-17 16:25:47 -0400

    reposition pitch elements

--- frontend/components/pitch/pitch.jsx
+++ frontend/components/pitch/pitch.jsx
@@ -11,21 +11,23 @@ const Pitch = () => {
             </div>
           </header>
           <div className='super-header'><h2>Travelers</h2></div>
-          <section className='pitch-item clearfix'>
-            <img src={window.girlIphone}></img>
-            <h3>Explore</h3>
-            <p>Take a road-trip through the countryside to see places you've never seen. Find freedom while meeting new people, or discover a new place to live.</p>
-          </section>
-          <section className='pitch-item clearfix'>
-            <img src={window.workImg} className='float-r'></img>
-            <h3>Fund Your Travels</h3>
-            <p>Find work while traveling. Whether a recent graduate or just needing a change of pace, now you can travel without the stress of draining your savings.</p>
-          </section>
-          <section className='pitch-item clearfix'>
-            <img src={window.playImg}></img>
-            <h3>Have an Adventure</h3>
-            <p>Choose from unique job opportunities and make new friends. Hang out with coworkers and plan your next adventure. Froliq and be free.</p>
-          </section>
+          <div className='pitch-item-container'>
+            <div className='pitch-item clearfix'>
+              <img src={window.girlIphone}></img>
+              <h3>Explore</h3>
+              <p>Take a road-trip through the countryside to see places you've never seen. Find freedom while meeting new people, or discover a new place to live.</p>
+            </div>
+            <div className='pitch-item clearfix'>
+              <img src={window.workImg} className='float-r'></img>
+              <h3>Fund Your Travels</h3>
+              <p>Find work while traveling. Whether a recent graduate or just needing a change of pace, now you can travel without the stress of draining your savings.</p>
+            </div>
+            <div className='pitch-item clearfix'>
+              <img src={window.playImg}></img>
+              <h3>Have an Adventure</h3>
+              <p>Choose from unique job opportunities and make new friends. Hang out with coworkers and plan your next adventure. Froliq and be free.</p>
+            </div>
+          </div>
         </div>
 
         <div className='col-lg-6 pitch-column'>
@@ -34,21 +36,23 @@ const Pitch = () => {
             </div>
           </header>
           <div className='super-header'><h2>Employers</h2></div>
-          <section className='pitch-item clearfix'>
-            <img src={window.girlLaptop}></img>
-            <h3>Create</h3>
-            <p>Make a free profile and customize your information to drive new prospective employees to your business.</p>
-          </section>
-          <section className='pitch-item clearfix'>
-            <img src={window.cafeGirl} className='float-r'></img>
-            <h3>Find Great People</h3>
-            <p>Review profiles and find new hires from around the country. Name your rate and find help when you need it.</p>
-          </section>
-          <section className='pitch-item clearfix'>
-            <img src={window.meeting}></img>
-            <h3>Never be Understaffed</h3>
-            <p>Skip the hassle of long-term hires and adapt to changing seasons. Rate and review applicants with ease.</p>
-          </section>
+          <div className='pitch-item-container'>
+            <div className='pitch-item clearfix'>
+              <img src={window.girlLaptop}></img>
+              <h3>Create</h3>
+              <p>Make a free profile and customize your information to drive new prospective employees to your business.</p>
+            </div>
+            <div className='pitch-item clearfix'>
+              <img src={window.cafeGirl} className='float-r'></img>
+              <h3>Find Great People</h3>
+              <p>Review profiles and find new hires from around the country. Name your rate and find help when you need it.</p>
+            </div>
+            <div className='pitch-item clearfix'>
+              <img src={window.meeting}></img>
+              <h3>Never be Understaffed</h3>
+              <p>Skip the hassle of long-term hires and adapt to changing seasons. Rate and review applicants with ease.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
